de1d501598d06a9b836e0e02cf084a19d5d42bfb
kimhyuncheol <kimhyuncheol@Khc.local>
2026-02-20 10:11:27 +0900

    ss

---
+++ chat.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+from dotenv import load_dotenv
+from llm import get_ai_response
+
+load_dotenv()
+
+st.set_page_config(page_title="소득세 챗봇", page_icon="🤖")
+
+st.title("🤖 소득세 챗봇")
+st.caption("소득세에 관련된 모든것을 답해드립니다!")
+
+if 'message_list' not in st.session_state:
+    st.session_state.message_list = []
+
+for message in st.session_state.message_list:
+    with st.chat_message("role"):
+        st.write(message["content"])
+        
+if user_qusetion := st.chat_input(placeholder="소득세에 관련된 궁금한 내용들을 말씀해주세요!"):
+    with st.chat_message("user"):
+        st.write(user_qusetion)
+    st.session_state.message_list.append({"role": "user", "content": user_qusetion})
+    
+    with st.spinner("답변을 생성하는 중입니다. 잠시만 기다려주세요"):
+        ai_response = get_ai_response(user_qusetion)
+    with st.chat_message("ai"):
+        st.write_stream(ai_response)
+    st.session_state.message_list.append({"role": "ai", "content": ai_response})
